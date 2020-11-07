81036480c6a20a344a268a7f2115d4d3156dbafd
Celso Ribeiro <celsofilho82@gmail.com>
2020-11-07 17:30:04 -0300

    add lógica de votação no controller

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,8 +1,38 @@
 class DestinationsController < ApplicationController
+  before_action :load_destinations, only: [:primeiro_destino, :segundo_destino]
+
   def home
   end
 
-  def index
+  def primeiro_destino
+    @first_choices = @destinations.first(2)
+  end
+
+  def up_vote
+    destiny = Destination.find(destination_params[:id])
+    voter = Votersession.first_or_create(session_id: request.session_options[:id])
+    destiny.upvote_by voter
+    if request.headers["referer"].include?("primeiro_destino")
+      
+      redirect_to segundo_destino_path
+    else
+      
+      render :home
+    end
+
+  end
+
+  def segundo_destino
+    @second_choices = @destinations.last(3)
+  end
+
+  private
+
+  def load_destinations
     @destinations = Destination.all
+  end
+
+  def destination_params
+    params.require(:destination).permit(:id)
   end
 end
