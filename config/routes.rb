8f6829c2e7e40be7cab90bfc8bfd05cab0aeb67c
Celso Ribeiro <celsofilho82@gmail.com>
2020-11-07 17:27:19 -0300

    add novas rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'destinations#home'
-  get 'index', to: 'destinations#index'
+  get 'primeiro_destino', to: 'destinations#primeiro_destino'
+  get 'segundo_destino', to: 'destinations#segundo_destino'
+  patch 'up-vote', to: 'destinations#up_vote'
 end
